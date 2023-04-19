f9a0e3c48765486f06ed947358dec80aa994ee6c
Ayman Hanif <aymanhanif@hotmail.co.uk>
2023-04-19 21:31:32 +0100

    Added user interface

--- TipCalculator/TipCalculator.swift
+++ TipCalculator/TipCalculator.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct TipCalculator: View {
+   
+    @State var billString = ""
+    @State var payeesString = ""
+    @State var tipPercentage:Double = 0
+
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Enter Bill Amount")
+                .font(.title)
+            
+            TextField("Enter bill amount", text: $billString)
+                .font(.callout)
+                .padding()
+                .frame(maxWidth: 300)
+                .cornerRadius(40)
+            
+            TextField("Enter number of payees", text: $payeesString)
+                .font(.callout)
+                .padding()
+                .frame(maxWidth: 300)
+                .cornerRadius(40)
+            
+            Text("Select Tip %")
+                .padding()
+            
+            Slider(value: $tipPercentage, in: 0...100, step: 1)
+            Text("\(Int(tipPercentage))%")
+            
+            
         }
         .padding()
     }
